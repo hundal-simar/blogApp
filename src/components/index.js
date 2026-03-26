f8b63379d56b9a933ca62e78f2a513bbbe902014
Simarpreet Kaur <ksimarpreet908@gmail.com>
2026-03-26 17:26:10 +0530

    added login and postcard components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Logo from "./logo";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import logoutBtn from "./Header/logoutBtn";
+import Button from "./Button";
+import Container from "./container/Container";
+
+export { Logo, Header, Footer, logoutBtn, Button, Container };
