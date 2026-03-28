29db19848aa96ef215a917b8cfd23cd4f093e595
Simarpreet Kaur <ksimarpreet908@gmail.com>
2026-03-28 13:28:15 +0530

    Postform added

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,12 @@ import Footer from "./Footer/Footer";
 import logoutBtn from "./Header/logoutBtn";
 import Button from "./Button";
 import Container from "./container/Container";
+import PostCard from "./PostCard";
+import Input from "./Input";
+import Select from "./Select";
+import Login from "./Login";
+import Signup from "./Signup";
+import RTE from "./RTE";
 
-export { Logo, Header, Footer, logoutBtn, Button, Container };
+
+export { Logo, Header, Footer, logoutBtn, Button, Container, PostCard, Input, Select, Login, Signup, RTE };
